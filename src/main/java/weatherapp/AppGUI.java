c483de29165f782821ba827d543d616d03c59c07
Lucas Eduardo <wenkcor13@gmail.com>
2023-12-10 18:21:29 -0300

    initial

---
+++ src/main/java/weatherapp/AppGUI.java
@@ -0,0 +1,30 @@
+package weatherapp;
+
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+
+public class AppGUI extends JFrame {
+    public AppGUI(){
+        //JFRAME: titulo do app
+        super("Clima");
+        //Swing: Mata o programa ao fechar (não fica rodando em segundo plano)
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        //tamanho da GUI: largura, altura.
+        setSize(450,650);
+        //centraliza a GUI.
+        setLocationRelativeTo(null);
+        //torna o gerenciador de layout null, para que se faça manualmente.
+        setLayout(null);
+        //torna a GUI não redimensionavel, uma vez que ela não é responsiva.
+        setResizable(false);
+    }
+
+    private void addGuiComponents(){
+        //area de busca
+        JTextField search = new JTextField();
+        //setBounds(x,y,width, height)
+        search.setBounds(15,15,350,45);
+        
+        
+    }
+}
