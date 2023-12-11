0b8e4b38afed8b8ee75e2bf1600200443df8b45e
Lucas Eduardo <wenkcor13@gmail.com>
2023-12-11 00:04:29 -0300

    Troca do classpath do java 17 para o java 21
    imagens da GUI e loaderImage add

--- src/main/java/weatherapp/AppGUI.java
+++ src/main/java/weatherapp/AppGUI.java
@@ -1,7 +1,19 @@
 package weatherapp;
 
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+
+import java.awt.image.BufferedImage;
+import java.awt.Font;
+
+
 
 public class AppGUI extends JFrame {
     public AppGUI(){
@@ -31,5 +43,62 @@ public class AppGUI extends JFrame {
         //adicionando a area de busca
         add(search);
 
+        //imagem do clima
+        JLabel conditionImage = new JLabel(loadImage("src/assets/rain.png"));
+        conditionImage.setBounds(0,125,450,217);
+        add(conditionImage);
+
+        //texto -> temperatura
+        JLabel temperatureText = new JLabel("32 C");
+        temperatureText.setBounds(0,350,450,54);
+        temperatureText.setFont(new Font("Dialog", Font.BOLD,48));
+        //centralizando o texto
+        temperatureText.setHorizontalAlignment(SwingConstants.CENTER);
+        add(temperatureText);
+
+        //descriçao do clima
+        JLabel conditionDesc = new JLabel("Chuvoso");
+        conditionDesc.setBounds(0,405,450,36);
+        conditionDesc.setFont(new Font("Dialog", Font.BOLD, 32));
+        conditionDesc.setHorizontalTextPosition(SwingConstants.CENTER);
+        add(conditionDesc);
+
+        //imagem da humidade
+        JLabel humidityImage = new JLabel(loadImage("src/assets/humidity.png"));
+        humidityImage.setBounds(15,500,74,65);
+        add(humidityImage);
+
+        //descriçao da humidade, utilizando html dentro do JLabel apenas para facilitar do bolder
+        JLabel humidityText = new JLabel("<html><b>Umidade</b> 100%</html>");
+        humidityText.setBounds(90, 500, 85, 55);
+        humidityText.setFont(new Font("Dialog", Font.PLAIN, 16));
+        add(humidityText);
+
+        JLabel windspeedImage = new JLabel(loadImage("src/assets/windspeed.png"));
+        windspeedImage.setBounds(220, 500, 75, 65);
+        add(windspeedImage);
+
+        JLabel windspeedText = new JLabel("<html><b>Velocidade do Vento</b> 23 Km/h</html>");
+        windspeedText.setBounds(310, 500, 115, 55);
+        windspeedText.setFont(new Font("Dialog", Font.PLAIN, 16));
+        add(windspeedText);
+        
+
+    }
+
+    // ImageIcon é usado para criar icones a partir de imagens
+    private ImageIcon loadImage(String resourcePath){
+        try{
+            //ler a imagem do caminho
+            BufferedImage image = ImageIO.read(new File(resourcePath));
+
+            //retorna o icone, assim o componente pode renderaliza-lo.
+            return new ImageIcon(image);
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+        // se a tentativa falhar, alem do catch retornar o caminho do erro, o metodo deverá retornar null
+        System.out.println("Não foi possivel carregar o recurso");
+        return null;
     }
 }
---
+++ src/main/java/weatherapp/AppLaucher.java
@@ -0,0 +1,17 @@
+package weatherapp;
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+public class AppLaucher extends JFrame{
+    public static void main(String[] args){
+        SwingUtilities.invokeLater(new Runnable(){ 
+            @Override
+            public void run(){
+
+                new AppGUI().setVisible(true);
+                
+            }
+        });
+    }
+}
